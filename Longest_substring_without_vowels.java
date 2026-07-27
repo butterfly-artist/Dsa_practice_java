ea02e3dfc3c0ae9c5ce78767397761125c4d61cb
Thotamadhulika <thota.madhulika05@gmail.com>
2026-07-27 21:49:22 +0530

    26.07.2026

---
+++ Longest_substring_without_vowels.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        Set<Character> set=new HashSet<>();
+        set.add('a');
+        set.add('i');
+        set.add('o');
+        set.add('u');
+        set.add('e');
+        while(T-->0){
+            String str=sc.next();
+            int count=0,maxset=0;
+            for(int i=0;i<str.length();i++){
+                char ch=str.charAt(i);
+                if(set.contains(ch)){
+                    maxset=Math.max(maxset,count);
+                    count=0;
+                }else{
+                    count++;
+                }
+            }
+            maxset=Math.max(maxset,count);
+            System.out.println(maxset);
+        }
+    }
+}
