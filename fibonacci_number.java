6e92bb7c2e9f23f46fe56efb9c86ddd884ed48b6
Thotamadhulika <thota.madhulika05@gmail.com>
2026-08-04 21:02:29 +0530

    04.08.2026

---
+++ fibonacci_number.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        if(N==0){ System.out.println(0); return;}
+        if(N==1){ System.out.println(1); return;}
+        int curr=0,prev2=0,prev1=1;
+        for(int i=2;i<=N;i++){
+            curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+            
+        }
+        System.out.println(curr);
+        
+    }
+}
