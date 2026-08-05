ea5e2fbc8ff41a02f7c07cb7e0b7065948409499
Thotamadhulika <thota.madhulika05@gmail.com>
2026-08-05 20:38:27 +0530

    codechef contest 05.08.2026

---
+++ presentation_CC_easy.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your  goes here
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int res=20-N;
+		System.out.println(res);
+
+	}
+}
