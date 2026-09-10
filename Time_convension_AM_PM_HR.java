7b86095ee953e862c39e8848f3a9b906d230dc38
THOTA MADHULIKA <thota.madhulika05@gmail.com>
2026-09-10 11:00:38 +0530

    10.09.2026

---
+++ Time_convension_AM_PM_HR.java
@@ -0,0 +1,14 @@
+public static String timeConversion(String s) {
+    // Write your code here
+    StringBuilder sb=new StringBuilder(s);
+    if(sb.charAt(8)=='P'){
+        if(!(sb.charAt(0)=='1'&&sb.charAt(1)=='2')){
+            sb.setCharAt(0, (char)((int)sb.charAt(0)+1));
+            sb.setCharAt(1, (char)((int)sb.charAt(1)+2));
+        }
+    }else if(sb.charAt(8)=='A'&&(sb.charAt(0)=='1'&&sb.charAt(1)=='2')){
+        sb.setCharAt(0, '0');
+        sb.setCharAt(1, '0');
+    }
+    return sb.substring(0,8);
+    }
